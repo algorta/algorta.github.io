4b7a7beafbb2815954d6b6575a5101c6f26878a4
algorta <rupertico@hotmail.com>
2015-07-19 18:53:28 +0200

    3rd commit
    seguro dental

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 var app=angular.module("app",[]);
   
-function PruebaController($scope) {
-  $scope.mensaje="Hola Mundo";
-   
-  $scope.cambiarMensaje=function() {
-    $scope.mensaje="Adios mundo cruel :-)";
+function SeguroController($scope) {
+  $scope.seguro={
+    nif:"",
+    nombre:"",
+    ape1:"",
+    edad:undefined,
+    sexo:"",
+    casado:false,
+    numHijos:undefined,
+    embarazada:false,
+    coberturas: {
+      oftalmologia:false,
+      dental:false,
+      fecundacionInVitro:false
+    },
+    enfermedades:{
+      corazon:false,
+      estomacal:false,
+      rinyones:false,
+      alergia:false,
+      nombreAlergia:""
+    },
+    fechaCreacion:new Date()
   }
-   
 }
